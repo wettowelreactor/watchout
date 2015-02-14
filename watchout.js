a2418d8d5ee2b0876e3adf8c1387638043181c6a
wettowelreactor <camille@rousselfamily.com>
2015-02-13 16:16:01 -0800

    added hero

--- watchout.js
+++ watchout.js
@@ -9,6 +9,38 @@ var Game = function(){
   };
 
   obj.initalize = function(){
+    this.addEnemies();
+    this.addPlayer();
+    intervalID = setInterval(this.gameTick.bind(this),1000)
+  }
+
+  obj.getRandomWidth = function() {
+    var width = (Math.random() * window.innerWidth) - 50;
+    width = width < 0 ? 0 : width;
+    return width;
+  };
+
+  obj.getRandomHeight = function() {
+    var height = (Math.random() * window.innerHeight) - 50;
+      height = height < 0 ? 0 : height;
+      return height;
+  };
+
+  obj.addPlayer = function() {
+    d3.selectAll(".hero")
+      .data(["hero"])
+      .enter()
+      .append('div')
+      .attr('class', 'hero')
+      .style('left', function() {
+        return this.getRandomWidth() + "px";
+      }.bind(obj))
+      .style('top', function() {
+        return this.getRandomHeight() + "px";
+      }.bind(obj));
+  };
+
+  obj.addEnemies = function(){
     var enemies = [];
     for(var i = 0; i < this.numOfEnemies ; i++){
       enemies[i] = i;
@@ -20,33 +52,24 @@ var Game = function(){
       .append('div')
       .attr('class', 'enemy')
       .style('left', function() {
-        var width = (Math.random() * window.innerWidth) - 50;
-        width = width < 0 ? 0 : width;
-        return width + "px"
-      })
-      .style('top', function() {
-        var height = (Math.random() * window.innerHeight) - 50;
-        height = height < 0 ? 0 : height;
-        return height + "px"
-      });
 
-  intervalID = setInterval(this.gameTick.bind(this),1000)
-  }
+        return this.getRandomWidth() + "px";
+      }.bind(obj))
+      .style('top', function() {
+        return this.getRandomHeight() + "px";
+      }.bind(obj));
+  };
 
   obj.gameTick = function(){
     d3.selectAll(".enemy")
     .transition()
     .duration(900)
     .style('left', function() {
-        var width = (Math.random() * window.innerWidth) - 50;
-        width = width < 0 ? 0 : width;
-        return width + "px"
-      })
+        return this.getRandomWidth() + "px";
+      }.bind(obj))
       .style('top', function() {
-        var height = (Math.random() * window.innerHeight) - 50;
-        height = height < 0 ? 0 : height;
-        return height + "px"
-      });
+        return this.getRandomHeight() + "px";
+      }.bind(obj));
 
   }
 
